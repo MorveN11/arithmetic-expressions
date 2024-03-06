ec819d62ff85bf402a8beb4f26d29527a0c73a19
MorveN11 <morales.patty.jose@gmail.com>
2024-03-06 09:16:49 -0400

    add Maybe to strToExpression

--- src/ArithmeticExpression.hs
+++ src/ArithmeticExpression.hs
@@ -5,18 +5,17 @@ type Operator = Char
 type Priority = Int
 
 data Expression
-  = Nill
-  | Val Int
+  = Val Int
   | Pow Expression Expression
   | Mult Expression Expression
   | Div Expression Expression
   | Sum Expression Expression
-  deriving (Show)
+  deriving (Show, Eq)
 
 priority :: Operator -> Priority
 priority c
   | c == '^' = 8
-  | c == '*' = 7
   | c == '/' = 7
+  | c == '*' = 7
   | c == '+' = 6
   | otherwise = 0
--- src/ExpressionEvaluator.hs
+++ src/ExpressionEvaluator.hs
@@ -4,7 +4,6 @@ import ArithmeticExpression (Expression (..), priority)
 import Data.Char (isDigit)
 
 evalExpression :: Expression -> Int
-evalExpression Nill = 0
 evalExpression (Val e) = e
 evalExpression (Pow e1 e2) = evalExpression e1 ^ evalExpression e2
 evalExpression (Mult e1 e2) = evalExpression e1 * evalExpression e2
@@ -14,42 +13,34 @@ evalExpression (Sum e1 e2) = evalExpression e1 + evalExpression e2
 operatorToExpression :: Char -> Expression -> Expression -> Expression
 operatorToExpression c e1 e2
   | c == '^' = Pow e1 e2
-  | c == '*' = Mult e1 e2
   | c == '/' = Div e1 e2
+  | c == '*' = Mult e1 e2
   | c == '+' = Sum e1 e2
-  | otherwise = Nill
+  | otherwise = error "Invalid operator"
 
-expressionToOperator :: Expression -> Char
-expressionToOperator Nill = ' '
-expressionToOperator (Val _) = ' '
-expressionToOperator (Pow _ _) = '^'
-expressionToOperator (Mult _ _) = '*'
-expressionToOperator (Div _ _) = '/'
-expressionToOperator (Sum _ _) = '+'
-
-strToExpression :: String -> Expression
-strToExpression [] = Nill
+strToExpression :: String -> Maybe Expression
+strToExpression [] = Nothing
 strToExpression xs
-  | null rest = Val (read num)
+  | null rest = Just num
   | otherwise =
       case strToExpression restAfterOp of
-        Nill -> strToExpression num
-        (Val e) -> operatorToExpression op (strToExpression num) (Val e)
-        (Sum e1 e2) -> Sum (operatorToExpression op (strToExpression num) e1) e2
-        (Mult e1 e2) -> do
-          if priority op >= priority (expressionToOperator (Mult e1 e2))
-            then Mult (operatorToExpression op (strToExpression num) e1) e2
-            else operatorToExpression op (strToExpression num) (Mult e1 e2)
-        (Div e1 e2) ->
-          if priority op >= priority (expressionToOperator (Div e1 e2))
-            then Div (operatorToExpression op (strToExpression num) e1) e2
-            else operatorToExpression op (strToExpression num) (Div e1 e2)
-        (Pow e1 e2) ->
-          if priority op >= priority (expressionToOperator (Pow e1 e2))
-            then Pow (operatorToExpression op (strToExpression num) e1) e2
-            else operatorToExpression op (strToExpression num) (Pow e1 e2)
+        Nothing -> Just num
+        Just (Val e) -> Just (operatorToExpression op num (Val e))
+        Just (Sum e1 e2) -> Just (Sum (operatorToExpression op num e1) e2)
+        Just (Mult e1 e2) -> do
+          if priority op >= priority '*'
+            then Just (Mult (operatorToExpression op num e1) e2)
+            else Just (operatorToExpression op num (Mult e1 e2))
+        Just (Div e1 e2) ->
+          if priority op >= priority '/'
+            then Just (Div (operatorToExpression op num e1) e2)
+            else Just (operatorToExpression op num (Div e1 e2))
+        Just (Pow e1 e2) ->
+          if priority op >= priority '^'
+            then Just (Pow (operatorToExpression op num e1) e2)
+            else Just (operatorToExpression op num (Pow e1 e2))
   where
-    num = takeWhile isDigit xs
+    num = Val (read (takeWhile isDigit xs))
     rest = dropWhile isDigit xs
     op = head rest
     restAfterOp = tail rest
